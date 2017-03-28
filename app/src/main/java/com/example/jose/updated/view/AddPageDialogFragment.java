0840cba7ae5dbecdda7a37789e9f21565369d3b1
Jose Garcia <jogarcia809@yahoo.com>
2017-03-28 18:52:28 -0400

    color change finished

--- app/src/main/java/com/example/jose/updated/view/AddPageDialogFragment.java
+++ app/src/main/java/com/example/jose/updated/view/AddPageDialogFragment.java
@@ -49,8 +49,7 @@ public class AddPageDialogFragment extends DialogFragment{
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        getDialog().getWindow()
-                .getAttributes().windowAnimations = R.style.DialogAnimation;
+        getDialog().getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
     }
 
     @Nullable
